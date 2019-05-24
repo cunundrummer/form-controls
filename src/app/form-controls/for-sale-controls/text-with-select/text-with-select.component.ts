8d59661763db9aa49804098ee062ae98d258955f
Unknown <sox_theguy@yahoo.com>
2019-05-24 13:12:57 -0400

    Removed unused components - too generic
    Basic-Controls 
    Form-Controls

---
+++ src/app/form-controls/for-sale-controls/text-with-select/text-with-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-with-select',
+  templateUrl: './text-with-select.component.html',
+  styleUrls: ['./text-with-select.component.css']
+})
+export class TextWithSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/form-controls/form-controls.module.ts
+++ src/app/form-controls/form-controls.module.ts
@@ -1,30 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BasicControlsComponent } from './basic-controls/basic-controls.component';
 import { MaterialModule } from '../material.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ForSaleControlsComponent } from './for-sale-controls/for-sale-controls.component';
 import { AdTitleComponent } from './common-controls/ad-title/ad-title.component';
 import { AdDescriptionComponent } from './common-controls/ad-description/ad-description.component';
 import { ErrorMessagesPipe } from './pipes/error-messages.pipe';
 import { ForSaleByComponent } from './for-sale-controls/for-sale-by/for-sale-by.component';
 import { ForSaleSellingForComponent } from './for-sale-controls/for-sale-selling-for/for-sale-selling-for.component';
 import { ForSaleConditionsComponent } from './for-sale-controls/for-sale-conditions/for-sale-conditions.component';
+import { TextWithSelectComponent } from './for-sale-controls/text-with-select/text-with-select.component';
 
 @NgModule({
   declarations: [
-    BasicControlsComponent,
-    ForSaleControlsComponent,
     AdTitleComponent,
     AdDescriptionComponent,
     ErrorMessagesPipe,
     ForSaleByComponent,
     ForSaleSellingForComponent,
-    ForSaleConditionsComponent
+    ForSaleConditionsComponent,
+    TextWithSelectComponent
   ],
   exports: [
-    BasicControlsComponent,
-    ForSaleControlsComponent,
     AdTitleComponent,
     AdDescriptionComponent,
     ForSaleByComponent,
